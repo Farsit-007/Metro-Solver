82f88a511df14a70d5758663e809518b97f365fb
Farsit-007 <robayatfarsit@gmail.com>
2025-10-12 01:22:34 +0600

    Updated

--- src/components/modules/Home/Hero.tsx
+++ src/components/modules/Home/Hero.tsx
@@ -11,13 +11,7 @@ export default function HeroSection() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <section className="relative overflow-hidden">
-      {/* <div className="absolute  z-50  inset-0">
-       
-        <div className="absolute-top-20 left-1/4 w-96 h-96 bg-purple-600/20 rounded-full blur-3xl "></div>
-        <div className="absolute bottom-20 right-1/4 w-96 h-96 bg-violet-600/20 rounded-full blur-3xl "></div>
-
-        
-      </div> */}
+     
 
       {/* Content Container */}
       <div className="relative max-w-7xl mx-auto  flex items-center ">
