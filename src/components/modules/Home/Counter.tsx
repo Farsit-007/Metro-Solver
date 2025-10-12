33583d935b92c09a1965095e7405228a6db564fc
Farsit-007 <robayatfarsit@gmail.com>
2025-10-13 02:29:43 +0600

    Updated

--- src/components/modules/Home/Counter.tsx
+++ src/components/modules/Home/Counter.tsx
@@ -4,7 +4,7 @@ const Counter = () => {
     <div className="max-w-7xl mx-auto py-20  relative overflow-x-hidden md:overflow-visible">
           <div className="absolute  -bottom-40 left-10 w-[750px] h-[750px] bg-purple-500/10 rounded-full blur-3xl "></div>
       <div className=" grid grid-cols-1 md:grid-cols-3 gap-8 md:gap-12">
-        {/* Current Clients */}
+
         <div className="text-center bg-[#25153694] py-4 rounded-2xl">
           <div className="text-5xl md:text-6xl font-bold text-white mb-2">
             150<span className="text-amber-400">+</span>
